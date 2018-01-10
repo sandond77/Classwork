88cfef0a2779a3593865b9a2ee4db2915f3697f7
sandu@ucdavis.edu <sandu@ucdavis.edu>
2018-01-09 21:19:02 -0800

    end of day 32

--- Week 11/Day 32/unsolved/unsolved/userprompt.js
+++ Week 11/Day 32/unsolved/unsolved/userprompt.js
@@ -1,7 +1,7 @@
 // Create a basic command line Node application using the inquirer package.
 // Your application should ask the user any five questions of your choosing.
 // The question set should include at least one:
-
+var inquirer = require('inquirer');
 //    - Basic input,
 //    - Password,
 //    - List,
@@ -14,3 +14,30 @@
 // Remember to be creative!
 
 // ========================================================================
+inquirer.prompt([
+	{ type: "input",
+	message: "Who Am I?",
+	name: "User"
+	},
+	{ type: "password",
+	message: "Create a password",
+	name: "Password"
+	},
+	{ type: "list",
+	message: "Choose your age group",
+	choices: ["Under 18", "Between 18 - 30", "Over 30"],
+	name: "Age Group"
+	},
+	{ type: "checkbox",
+	message: "Gender",
+	choices: ["Male","Female"],
+	name: "Gender"
+	},
+	{ type: "confirm",
+	message: "Is all of the above correct?",
+	name: "Confirmation",
+	default: true
+	}
+]).then(function (response){
+	console.log("Congrats you have wasted your time with my survey")
+})
