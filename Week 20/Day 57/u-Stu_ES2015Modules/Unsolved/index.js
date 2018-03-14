dc4fa2f9afcf4f273d31eae68840a1c43e79d2d3
sandu@ucdavis.edu <sandu@ucdavis.edu>
2018-03-13 21:27:42 -0700

    end of day 57

---
+++ Week 20/Day 57/u-Stu_ES2015Modules/Unsolved/index.js
@@ -0,0 +1,7 @@
+// Add code to import the spriteLogger function as well as each of the 3 sprites (happyFace, stickFigure, star)
+import spriteLogger from "./spriteLogger"
+import {happyFace, stickFigure, star} from "./sprites"
+
+spriteLogger(happyFace);
+spriteLogger(stickFigure);
+spriteLogger(star);
