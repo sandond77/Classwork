235d4215efa1d6ee772019aad59ee6573fa8af07
sandu@ucdavis.edu <sandu@ucdavis.edu>
2018-03-15 21:08:08 -0700

    end of jsxvariables activity

--- Week 20/Day 58/react practice/src/App.js
+++ Week 20/Day 58/react practice/src/App.js
@@ -1,22 +1,6 @@
 import React from "react";
+import JSXVariables from "./components/JSXVariables";
 
-const App = () => (
-	<div class="container">
-		<div class="jumbotron">
-		  <div class="container">
-			  <h1>I'M HUGE TEXT </h1>
-			  <p>asdfsadfsadfasdfasdf.edfasdfsdfsadfsadfsdaf</p>
-			  <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a></p>
-		  </div>
-		</div>
-		<div>
-			<ul class="nav nav-pills">
-			  <li role="presentation" class="active"><a href="#">Home</a></li>
-			  <li role="presentation"><a href="#">Profile</a></li>
-			  <li role="presentation"><a href="#">Messages</a></li>
-			</ul>
-		</div>
-	</div>
-)
+const App = () => <JSXVariables />;
 
 export default App;
---
+++ Week 20/Day 58/react practice/src/components/JSXVariables.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const name = "Sandon";
+const length = name.length;
+
+const JSXVariables = () => (
+  <div className="main-container">
+    <div className="container">
+      <div className="jumbotron">
+        <h1>Hi! My name is {name}</h1>
+        <h2>My name has {length} letters</h2>
+        <h2>I think React is very convenient</h2>
+        <h2>My name without vowels is:
+    	</h2>
+ 
+      </div>
+    </div>
+  </div>
+);
+
+export default JSXVariables;
