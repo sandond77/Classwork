1ade55aba76d6ba06a1db84bcc2c6071b0e1a260
sandu@ucdavis.edu <sandu@ucdavis.edu>
2018-03-15 20:56:14 -0700

    completed react-bootstrap activity

--- Week 20/Day 58/react practice/src/App.js
+++ Week 20/Day 58/react practice/src/App.js
@@ -1,6 +1,22 @@
 import React from "react";
-import HelloReact from "./components/HelloReact";
 
-const App = () => <HelloReact />;
+const App = () => (
+	<div class="container">
+		<div class="jumbotron">
+		  <div class="container">
+			  <h1>I'M HUGE TEXT </h1>
+			  <p>asdfsadfsadfasdfasdf.edfasdfsdfsadfsadfsdaf</p>
+			  <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a></p>
+		  </div>
+		</div>
+		<div>
+			<ul class="nav nav-pills">
+			  <li role="presentation" class="active"><a href="#">Home</a></li>
+			  <li role="presentation"><a href="#">Profile</a></li>
+			  <li role="presentation"><a href="#">Messages</a></li>
+			</ul>
+		</div>
+	</div>
+)
 
 export default App;
