b4e5ba896d5a8c7a5bedc2fc126cd2b861b8a4a7
sandu@ucdavis.edu <sandu@ucdavis.edu>
2018-03-17 11:41:13 -0700

    added solution to props review

--- Week 20/Day 59/day59-react/src/App.js
+++ Week 20/Day 59/day59-react/src/App.js
@@ -1,6 +1,15 @@
 import React from "react";
-import Calculator from "./components/Calculator";
+import Wrapper from "./components/Wrapper";
+import Title from "./components/Title";
+import friends from "./friends.json";
 
-const App = () => <Calculator />;
+const App = () => (
+  <Wrapper>
+    <Title>Friends List</Title>
+    <SpongeBobCard />
+    <MrKrabsCard />
+    <SquidwardCard />
+  </Wrapper>
+);
 
 export default App;
