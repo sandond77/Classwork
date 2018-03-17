a4297f91dcceba86e82f18606dc2185c5d9a9da3
sandu@ucdavis.edu <sandu@ucdavis.edu>
2018-03-17 13:37:48 -0700

    added solution for friendrefractor

--- Week 20/Day 59/day59-react/src/App.js
+++ Week 20/Day 59/day59-react/src/App.js
@@ -1,6 +1,67 @@
-import React from "react";
-import Counter from "./components/Counter";
+import React, { Component } from "react";
+import FriendCard from "./components/FriendCard";
+import Wrapper from "./components/Wrapper";
+import friends from "./friends.json";
+import "./App.css";
 
-const App = () => <Counter />;
+// const App = () => (
+//   <Wrapper>
+//     <h1 className="title">Friends List</h1>
+//     <FriendCard
+//       name={friends[0].name}
+//       image={friends[0].image}
+//       occupation={friends[0].occupation}
+//       location={friends[0].location}
+//     />
+//     <FriendCard
+//       name={friends[1].name}
+//       image={friends[1].image}
+//       occupation={friends[1].occupation}
+//       location={friends[1].location}
+//     />
+//     <FriendCard
+//       name={friends[2].name}
+//       image={friends[2].image}
+//       occupation={friends[2].occupation}
+//       location={friends[2].location}
+//     />
+
+//   </Wrapper>
+// );
+
+// export default App;
+
+
+  // <ul className="list-group">
+  //   {props.groceries.map(item => (
+  //     <li className="list-group-item" key={item.id}>
+  //       {item.name}
+  //     </li>
+  //   ))}
+  // </ul>
+
+class App extends Component{
+  state = {
+    friends
+  };
+
+  render(){
+    return (
+      <Wrapper>
+        <h1 className="title">Friends List</h1>
+          {this.state.friends.map(friend =>(
+            <FriendCard 
+              key={friend.id}
+              name={friend.name}
+              image={friend.image}
+              occupation={friend.occupation}
+              location={friend.location}
+            />
+        ))} 
+      </Wrapper>
+    );
+  }
+}
+  
 
 export default App;
--- Week 20/Day 59/day59-react/src/index.js
+++ Week 20/Day 59/day59-react/src/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import "./index.css";
+import friends from "./friends.json";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App friends={friends}/>, document.getElementById("root"));
