b1802fb28fc2c24ae8abdb5ce643644ad99c7e68
sandu@ucdavis.edu <sandu@ucdavis.edu>
2018-03-20 18:36:20 -0700

    start of week 21/day 60

--- Week 20/Day 59/day59-react/src/index.js
+++ Week 20/Day 59/day59-react/src/index.js
@@ -4,4 +4,5 @@ import App from "./App";
 import "./index.css";
 import friends from "./friends.json";
 
-ReactDOM.render(<App friends={friends}/>, document.getElementById("root"));
+ReactDOM.render(<App friends={friends}/>, document.getElementById("root"))
+  
