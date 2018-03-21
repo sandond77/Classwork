9c812199f5aeeaa20f4bccc2cf3c845cc6e7a4c2
sandu@ucdavis.edu <sandu@ucdavis.edu>
2018-03-20 20:21:28 -0700

    end of break, start of ex5

--- Week 21/Day 60/solved ex3/Solved/src/components/OmdbContainer.js
+++ Week 21/Day 60/solved ex3/Solved/src/components/OmdbContainer.js
@@ -13,9 +13,10 @@ class OmdbContainer extends Component {
     search: ""
   };
 
-componentDidMount(){
-  this.searchMovies("The Matrix");
-};
+  // When this component mounts, search for the movie "The Matrix"
+  componentDidMount() {
+    this.searchMovies("The Matrix");
+  }
 
   searchMovies = query => {
     API.search(query)
@@ -24,12 +25,14 @@ componentDidMount(){
   };
 
   handleInputChange = event => {
-    const {name,value} = event.target;
+    const value = event.target.value;
+    const name = event.target.name;
     this.setState({
-      [name]:value
-    })
+      [name]: value
+    });
   };
 
+  // When the form is submitted, search the OMDB API for the value of `this.state.search`
   handleFormSubmit = event => {
     event.preventDefault();
     this.searchMovies(this.state.search);
@@ -43,13 +46,15 @@ componentDidMount(){
             <Panel
               heading={this.state.result.Title || "Search for a Movie to Begin"}
             >
-              <MovieDetail
-                title={this.state.result.Title}
-                src={this.state.result.Poster}
-                director={this.state.result.Director}
-                genre={this.state.result.Genre}
-                released={this.state.result.Released}
-              />
+              {this.state.result.Title
+                ? <MovieDetail
+                  title={this.state.result.Title}
+                  src={this.state.result.Poster}
+                  director={this.state.result.Director}
+                  genre={this.state.result.Genre}
+                  released={this.state.result.Released}
+                />
+                : <h3>No Results to Display</h3>}
             </Panel>
           </Col>
           <Col size="md-4">
